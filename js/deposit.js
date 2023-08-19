9bae635801a509000b024c4661de98f660f80e0b
MHShihan <mahmodul.no.1@gmail.com>
2023-08-19 11:56:21 +0600

    some change has been made

--- js/deposit.js
+++ js/deposit.js
@@ -1,12 +1,19 @@
 document.getElementById("deposit-btn").addEventListener("click", function () {
+  // Take the input field value and turn it into a floating number
   const depositField = document.getElementById("deposit-field");
   const newDepositString = depositField.value;
+  const newDeposit = parseFloat(newDepositString);
+
+  depositField.value = "";
+  if (Number.isNaN(newDeposit)) {
+    alert("Please Provide Valid  Number");
+    return;
+  }
 
   const depositAmountElement = document.getElementById("deposit-amount");
   const previousDepositAmountString = depositAmountElement.innerText;
   const previousDepositAmount = parseFloat(previousDepositAmountString);
-  depositAmountElement.innerText = newDepositString;
-  const newDeposit = parseFloat(newDepositString);
+  depositAmountElement.innerText = newDeposit;
 
   const currentDeposit = previousDepositAmount + newDeposit;
   depositAmountElement.innerText = currentDeposit;
@@ -14,12 +21,7 @@ document.getElementById("deposit-btn").addEventListener("click", function () {
   const balanceTotalAmount = document.getElementById("balance-amount");
   const previousTotalBalanceString = balanceTotalAmount.innerText;
   const previousTotalBalance = parseFloat(previousTotalBalanceString);
+
   const currentBalance = previousTotalBalance + newDeposit;
   balanceTotalAmount.innerText = currentBalance;
-
-  if (Number.isNaN(newDeposit)) {
-    alert("Please Provide Valid  Number");
-  }
-
-  depositField.value = "";
 });
